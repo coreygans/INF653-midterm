af32dd057ad551dd3dee9fc68523a0ad0f549172
Corey <coreygans@gmail.com>
2023-03-12 19:00:56 -0700

    Added headers to the GET all categories

--- api/categories/get.php
+++ api/categories/get.php
@@ -1,6 +1,12 @@
 <?php
 
 if(empty($_GET['id'])){ 
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
   // Category read query
   $result = $category->read();
 
