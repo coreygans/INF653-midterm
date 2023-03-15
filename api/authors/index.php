b29975ddb419ec529866463f84378659766a682f
Corey <coreygans@gmail.com>
2023-03-14 20:47:01 -0700

    Added author endpoints

---
+++ api/authors/index.php
@@ -0,0 +1,37 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+  }
+include_once '../../config/Database.php';
+include_once '../../model/Author.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate category object
+$author = new Author($db);
+
+
+
+if($method === 'POST') {
+  include_once 'post.php';
+}
+
+if($method === 'GET') {
+  include_once 'get.php';
+}
+if(($_SERVER['REQUEST_METHOD'] === 'PUT')) {
+  include_once 'update.php';
+ }
+
+
+if(($_SERVER['REQUEST_METHOD'] === 'DELETE')) {
+ include_once 'delete.php';
+}
---
+++ api/authors/post.php
@@ -0,0 +1,46 @@
+<?php
+
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json, true);
+
+
+
+$name = $data['category'];
+
+if(empty($name)) {
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+  echo json_encode(
+    array('message' => 'Missing Required Parameters')
+  );
+}
+
+elseif(!empty($name)) {
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+   
+
+  $category->name = $name;  
+
+    // Create category
+  $category->create();
+ 
+}
+else {
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+  echo json_encode(
+    array('message' => 'Missing Required Parameters')
+  );
+}
