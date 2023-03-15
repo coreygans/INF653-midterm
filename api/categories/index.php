087ee38f63273666f4229f0fd7088eb5198b912d
Corey <coreygans@gmail.com>
2023-03-14 20:14:36 -0700

    refactored index.php for categories

--- api/categories/index.php
+++ api/categories/index.php
@@ -1,7 +1,15 @@
 <?php
 
-  include_once '../../config/Database.php';
-  include_once '../../model/Category.php';
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    exit();
+  }
+include_once '../../config/Database.php';
+include_once '../../model/Category.php';
 
 // Instantiate DB & connect
 $database = new Database();
@@ -10,12 +18,7 @@ $db = $database->connect();
 // Instantiate category object
 $category = new Category($db);
 
-$method = $_SERVER['REQUEST_METHOD'];
-if ($method === 'OPTIONS') {
-  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-  header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-  exit();
-}
+
 
 if($method === 'POST') {
   include_once 'post.php';
