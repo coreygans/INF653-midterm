17fea0b193d123f25895da1ffc3bcaac7311439d
Corey <coreygans@gmail.com>
2023-03-14 20:22:46 -0700

    Added missing Post response in case wrong param is sent

--- api/categories/post.php
+++ api/categories/post.php
@@ -34,3 +34,13 @@ elseif(!empty($name)) {
   $category->create();
  
 }
+else {
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+  echo json_encode(
+    array('message' => 'Missing Required Parameters')
+  );
+}
