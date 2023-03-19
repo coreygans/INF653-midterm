8ffc38c31fbcb3db3b25bd732bd2c9eaf82b6230
Corey <coreygans@gmail.com>
2023-03-18 21:41:55 -0500

    Updated error logic

--- api/quotes/post.php
+++ api/quotes/post.php
@@ -5,19 +5,11 @@ $json = file_get_contents('php://input');
 // Converts it into a PHP object
 $data = json_decode($json, true);
 
-$quotation = null;
-$author = null;
-$category = null;
-
-$quotation = $data['quote'];
-$author = $data['author_id'];
-$category = $data['category_id'];
+$quotation = $data['quote'] ?? null;
+$author = $data['author_id'] ?? null;
+$category = $data['category_id'] ?? null;
 
 if (empty($quotation) || empty($author) || empty($category)) {
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: POST');
-  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
   echo json_encode(
     array('message' => 'Missing Required Parameters')
