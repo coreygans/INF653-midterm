dd56d76292c2048498d5f223e3f28be65f885b45
Corey <coreygans@gmail.com>
2023-03-18 21:43:03 -0500

    Fixing error messages in update

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -5,21 +5,13 @@ $json = file_get_contents('php://input');
 // Converts it into a PHP object
 $data = json_decode($json, true);
 
-$id = null;
-$quotation = null;
-$author = null;
-$category = null;
 
-$id = $data['id'];
-$quotation = $data['quote'];
-$author = $data['author_id'];
-$category = $data['category_id'];
+$id = $data['id'] ?? null;
+$quotation = $data['quote'] ?? null;
+$author = $data['author_id'] ?? null;
+$category = $data['category_id'] ?? null;
 
 if (empty($id) || !is_int($id) || empty($quotation) || empty($author) || !is_int($author) || empty($category) || !is_int($category)) {
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
-  header('Access-Control-Allow-Methods: PUT');
-  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
   echo json_encode(
     array('message' => 'Missing Required Parameters')
   );
